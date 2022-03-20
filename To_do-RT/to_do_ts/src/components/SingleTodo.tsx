0a169c2fb3474f6e71d0ad6dec792c0ac644f38d
sam.02425 <saumilpatel2425@gmail.com>
2022-03-20 17:36:20 -0500

    update

--- To_do-RT/to_do_ts/src/components/SingleTodo.tsx
+++ To_do-RT/to_do_ts/src/components/SingleTodo.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React,{useEffect, useRef, useState} from 'react'
 import { AiFillEdit,AiOutlineDelete } from 'react-icons/ai';
 import { MdDoneOutline } from 'react-icons/md';
 import {Todo } from '../model'
 import './styles.css'
+import TodoList from './TodoList';
 
 
 type props = {
@@ -12,19 +13,51 @@ type props = {
 
 }
 const SingleTodo = ({todo, todos, setTodos}: props) => {
+
+    const [edit, setEdit] = useState<boolean>(false)
+    const [editTodo, setEditTodo] = useState<string>(todo.todo)
+
+    const handleDone = (id: number) => {
+        setTodos(todos.map((todo)=>todo.id === id ? { ...todo, isDone:!todo.isDone}:todo))
+    }
+
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo)=>todo.id !== id))
+    }
+
+    const handleEdit = (e:React.FormEvent, id: number) => {
+        e.preventDefault();
+
+        setTodos(todos.map((todo)=> (todo.id===id?{...todo,todo:editTodo}:todo)))
+        setEdit(false)
+    }
+
+    useEffect(() => { 
+        inputRef.current?.focus();
+    }, [edit])
+    
+    const inputRef = useRef<HTMLInputElement>(null)
+
   return (
-    <form className='todos__single'>
-        <span className='todos__single--text'>
-            {todo.todo}
-        </span>
+    <form className='todos__single' onSubmit={(e)=>handleEdit(e, todo.id)}>
+        {edit? (
+            <input value={editTodo} onChange={(e)=>setEditTodo(e.target.value)} className='todos__single--text'/>
+            ): todo.isDone ? (
+                <s className='todos__single--text'>
+                {todo.todo}
+               </s>
+           ): ( <span className='todos__single--text'>
+               {todo.todo}
+               </span>)}
+
         <div >
-            <span className="icon">
+            <span className="icon" onClick={()=>{ if(!edit && !todo.isDone){setEdit(!edit)}}}>
                 <AiFillEdit/>
             </span>
-            <span className="icon">
+            <span className="icon" onClick={()=>handleDelete(todo.id)}>
                 <AiOutlineDelete/>
             </span>
-            <span className="icon">
+            <span className="icon" onClick={()=>handleDone(todo.id)}>
                 <MdDoneOutline/>
             </span>
         </div>
--- To_do-RT/to_do_ts/src/components/TodoList.tsx
+++ To_do-RT/to_do_ts/src/components/TodoList.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import {Todo} from '../model'
 import SingleTodo from './SingleTodo'
 import './styles.css'
+// import { Droppable } from 'react-beautiful-dnd'
+
 
 interface props {
     todos: Array<Todo>
     setTodos : React.Dispatch<React.SetStateAction<Array<Todo>>>
+    setCompletedTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>
+    CompletedTodos: Array<Todo>
 }
 
-const TodoList: React.FC<props> = ({todos, setTodos}) => {
+const TodoList: React.FC<props> = ({todos, setTodos, setCompletedTodos, CompletedTodos}) => {
   return (
     <div className='todos'>
         {todos.map((todo) => (
