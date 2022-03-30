f30bab3d920c97021ed905948e5204c68eada8e7
sam.02425 <saumilpatel2425@gmail.com>
2022-03-30 16:58:04 -0500

    question 3

--- 3/index.js
+++ 3/index.js
@@ -1,38 +1,55 @@
+// function createRoutes(value){
+//   inputNum = document.getElementById('inputNumber').value
+//   var invalues =  Array.from({length: inputNum}, (_, i) => i + 1)
+//     return invalues;
+// }
+
+
 function generatePermutations(Arr){
-    var permutations = [];
-    var A = Arr.slice();
+  var permutations = [];
+  var A = Arr.slice();
+
   
-    function swap(a,b){
-      var tmp = A[a];
-      A[a] = A[b];
-      A[b] = tmp;
-    }
-  
-    function generate(n, A){
-      if (n == 1){
-        permutations.push(A.slice());
-      } else {
-        for(var i = 0; i <= n-1; i++) {
-          generate(n-1, A);
-          swap(n % 2 == 0 ? i : 0 ,n-1);
-        }
+  function swap(a,b){
+    var tmp = A[a];
+    A[a] = A[b];
+    A[b] = tmp;
+  }
+
+  function generate(n, A){
+    if (n == 1){
+      permutations.push(A.slice());
+    } else {
+      for(var i = 0; i <= n-1; i++) {
+        generate(n-1, A);
+        swap(n % 2 == 0 ? i : 0 ,n-1);
       }
     }
-    generate(A.length, A);
-    return permutations;
   }
-  
-  
-  function generateCityRoutes(cities){
-    var pems = generatePermutations(cities.slice(1));
-    for (var i = 0; i < pems.length; i++){
-      pems[i].unshift(cities[0]);
-      pems[i].push(cities[0]);
-    }
-    return pems;
+  generate(A.length, A);
+  return permutations;
+}
+
+
+function generateCityRoutes(cities){
+  var pems = generatePermutations(cities.slice(1));
+  for (var i = 0; i < pems.length; i++){
+    pems[i].unshift(cities[0]);
+    pems[i].push(cities[0]);
   }
+  return pems;
+}
+
   
+  cityRoutes = generatePermutations([1,2,3,4,5,6,7])
+  // permutations = generatePermutations(numberOfRoutes)
+  // displayPermutations= permutations.join('<br>')
+  displayCityRoutes= cityRoutes.join('<br>')
+
+  // console.log(permutations);
   
-  console.log(generatePermutations([1,2,3]));
-  
-  console.log(generateCityRoutes([0,1,2,3]));
+//   console.log(cityRoutes);
+
+
+//   // document.getElementById('cityroutes').innerHTML = displayPermutations
+  document.getElementById('routes').innerHTML = displayCityRoutes
